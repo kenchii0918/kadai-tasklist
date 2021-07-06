1a811c7dd7613594e91831e3c8bc36b83d3b16b3
kenta-nishimura <kenchii365@gmail.com>
2021-07-06 06:07:58 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $task->id }} の詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    {!! link_to_route('tasks.edit', 'タスク編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    <div class="navbar-nav mr-auto">
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    </div>
+    
+@endsection
