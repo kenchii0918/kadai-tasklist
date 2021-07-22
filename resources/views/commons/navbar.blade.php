1f158653212f55cccd388f127baf7e62ed7887a8
kenta-nishimura <kenchii365@gmail.com>
2021-07-22 06:53:13 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,7 +10,14 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class='nav-item'>{!! link_to_route('tasks.create', '新規タスク追加', [], ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check())
+                    <li class='nav-item'>{!! link_to_route('tasks.create', '新規タスク追加', [], ['class' => 'nav-link']) !!}</li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
